95b715264c2946f0b4e513ab8efc59543d0b5c66
zeshalondrag <isip_a.v.samarskiy@mpt.ru>
2024-04-29 21:11:43 +0300

    Устал

---
+++ Calendar - MVVM/Utilites/De(Serealize).cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar___MVVM.Utilites
+{
+    class De_Serealize_
+    {
+    }
+}
---
+++ Calendar - MVVM/Views/ChoicePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calendar___MVVM.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ChoicePage.xaml
+    /// </summary>
+    public partial class ChoicePage : Page
+    {
+        public ChoicePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Page_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
